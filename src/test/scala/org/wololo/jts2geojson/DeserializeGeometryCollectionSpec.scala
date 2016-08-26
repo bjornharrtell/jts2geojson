5dd9c69913289e76bcfed1551410a3fb6b45254b
Jeffrey Erikson <jeffrey.erikson@digitalglobe.com>
2016-08-26 15:17:09 -0400

    Add Jackson annotations for handling Geometry subclasses in GeometryCollection

---
+++ src/test/scala/org/wololo/jts2geojson/DeserializeGeometryCollectionSpec.scala
@@ -0,0 +1,33 @@
+package org.wololo.jts2geojson
+
+import java.util.HashMap
+
+import org.scalatest.WordSpec
+import org.wololo.geojson._
+
+class DeserializeGeometryCollectionSpec extends WordSpec {
+    "GeoJSONFactory" when {
+      "parsing GeoJSON to object" should {
+        val geometry = new Point(Array(1, 1))
+        val properties = new HashMap[String, Object]()
+        properties.put("test", 1.asInstanceOf[Object])
+        val feature = new Feature(geometry, properties)
+
+
+        "deserialize GeometryCollection successfully" in {
+          val geoJSON = """{"type": "GeometryCollection", "geometries": [{"type": "Point",  "coordinates": [1.1, 2.2] }]}"""
+          val json = GeoJSONFactory.create(geoJSON)
+          assert(json.isInstanceOf[GeometryCollection])
+          val gc = json.asInstanceOf[GeometryCollection]
+          assert(gc.getGeometries.nonEmpty)
+          val g = gc.getGeometries.head
+          assert(g != null)
+          assert(g.isInstanceOf[Point])
+          val point = g.asInstanceOf[Point]
+          assert(point.getCoordinates.toSeq == Seq(1.1, 2.2))
+        }
+
+      }
+
+    }
+}
